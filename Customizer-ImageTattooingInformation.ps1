a059d6fcab7dbe0922303731ffa958ba275151d0
vmc-comp-23 <134539350+vmc-comp-23@users.noreply.github.com>
2023-05-24 14:51:26 -0500

    Delete Customizer-ImageTattooingInformation.ps1

--- Customizer-ImageTattooingInformation.ps1
+++
@@ -1,3 +0,0 @@
-mkdir c:\buildArtifacts
-new-item c:\buildArtifacts\imagetattooinginfo.txt
-set-content c:\buildArtifacts\imagetattooinginfo.txt "Original source of the image - Windows Server 2019 Datacenter published by Microsoft. Publish date - 5/24/23. Created by - Migdalia M. VM image definition - vmWinServer19. Vm image version - 1.2.0"
