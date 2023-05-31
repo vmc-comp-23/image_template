e928caabc5f8bf1c9a24202b63eb15f28b0560c7
vmc-comp-23 <134539350+vmc-comp-23@users.noreply.github.com>
2023-05-31 16:13:57 -0500

    Update Validator_Linux_Script.sh

--- Validator_Linux_Script.sh
+++ Validator_Linux_Script.sh
@@ -1,2 +1,9 @@
 #!/bin/sh -e
-
+# Add preview banner to MOTD
+cat >> /etc/motd << EOF
+*******************************************************
+**            This VM was built from the:            **
+**      !! AZURE VM IMAGE BUILDER Custom Image !!    **
+**         You have just been Customized :-)         **
+*******************************************************
+EOF
