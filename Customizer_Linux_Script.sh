f11f47f0ed598e8cd7270e62dd9b7eab64e6eec6
vmc-comp-23 <134539350+vmc-comp-23@users.noreply.github.com>
2023-05-31 16:15:29 -0500

    Update Customizer_Linux_Script.sh

--- Customizer_Linux_Script.sh
+++ Customizer_Linux_Script.sh
@@ -1,1 +1,8 @@
 #!/bin/sh -e
+mkdir /etc/buildArtifacts
+sudo chmod 777 /etc/buildArtifacts
+cd /etc/buildArtifacts
+touch image_tattooing.txt
+sudo chmod 777 image_tattooing.txt
+vmdetails="Original source of the image = Ubuntu Server 20.04 LTS Publisher = Canonical Date created = 5/31/23 Created by = Migdalia M VM image definition = vmLinuxServer20.04 VM image version = 2.0.1"
+echo "$vmdetails" >> image_tattooing.txt
