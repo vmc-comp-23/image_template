a6178e9bb53c7b761c1c90118febe3d77071408b
vmc-comp-23 <134539350+vmc-comp-23@users.noreply.github.com>
2023-05-31 12:46:37 -0500

    Update Validator_Linux_Script.sh

--- Validator_Linux_Script.sh
+++ Validator_Linux_Script.sh
@@ -1,2 +1,4 @@
 #!/bin/sh -e
-
+cd /etc/buildArtifacts
+vmdetails="Original source of the image = Ubuntu Server 20.04 LTS Publisher = Canonical Date created = 5/31/23 Created by = Migdalia M VM image definition = vmLinuxServer20.04 VM image version = 2.0.1"
+echo "$vmdetails" >> image_tattooing.txt
