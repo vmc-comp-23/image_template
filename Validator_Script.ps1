98fa7e1d3795ea14d711e8c25d50ff695ba90b19
vmc-comp-23 <134539350+vmc-comp-23@users.noreply.github.com>
2023-07-11 21:27:49 -0500

    Update Validator_Script.ps1

--- Validator_Script.ps1
+++ Validator_Script.ps1
@@ -1,1 +1,1 @@
-Test-Path -path c:\buildArtifacts\image_tattooing_information.csv -PathType Leaf | Out-File -FilePath c:\buildArtifacts\information_check.txt
+Test-Path -Path c:\buildArtifacts\image_tattooing_information.csv -PathType Leaf | Out-File -FilePath c:\buildArtifacts\information_check.txt
