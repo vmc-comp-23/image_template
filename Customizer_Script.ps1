c0d8fbdb2a2d8eeded70e98371fcbf24d016088b
vmc-comp-23 <134539350+vmc-comp-23@users.noreply.github.com>
2023-07-11 22:45:23 -0500

    Update Customizer_Script.ps1

--- Customizer_Script.ps1
+++ Customizer_Script.ps1
@@ -2,7 +2,7 @@ mkdir c:\buildArtifacts
 $vmdetails = [ordered]@{
     'Original source of the image' = 'Windows Server 2019 Datacenter'
     'Publisher' = 'Microsoft'
-    'Date created' = '7/11/23'
+    'Date created' = '7/12/23'
     'Created by' = 'Migdalia M'
     'VM image definition' = 'vmWinServer19'
     'VM image version' = '1.5.0'
