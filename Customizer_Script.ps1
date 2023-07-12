ac661a348d513979be150243c3869cbafab51d29
vmc-comp-23 <134539350+vmc-comp-23@users.noreply.github.com>
2023-07-11 21:46:48 -0500

    Update Customizer_Script.ps1

--- Customizer_Script.ps1
+++ Customizer_Script.ps1
@@ -2,9 +2,9 @@ mkdir c:\buildArtifacts
 $vmdetails = [ordered]@{
     'Original source of the image' = 'Windows Server 2019 Datacenter'
     'Publisher' = 'Microsoft'
-    'Date created' = '5/25/23'
+    'Date created' = '7/11/23'
     'Created by' = 'Migdalia M'
     'VM image definition' = 'vmWinServer19'
-    'VM image version' = '1.4.0'
+    'VM image version' = '1.5.0'
     }
 $vmdetails.GetEnumerator() | Select Key, Value | Export-CSV -path c:\buildArtifacts\image_tattooing_information.csv
